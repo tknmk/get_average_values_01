28ead8e26ce28569bea619e078a2888947e32714
takanami.kodai <kohdai.takanami@gmail.com>
2019-09-03 16:19:51 +0900

    Add new file & update README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Get average
+
+get average by sum method and numpy from a list
+
+リストから、sum関数とnumpyを使用して平均を出力します。
+
+
+# Get Standard deviation & Median
+
+get Standard deviation and Median by numpy.
+
+numpyを使用して、標準偏差と中央地を出力します。
---
+++ get_combination.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+a = np.random.normal(0,1,1000000)
+
+
+# standard deviation
+print('standard deviation is:')
+print(np.std(a))
+
+# Median
+print('median is:')
+print(np.median(a))
+
+
+#if __name__ == '__main__':
+#    main()
+
